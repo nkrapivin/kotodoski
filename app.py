67294660aea985cec752289d7f73b51702836448
Nikita Krapivin <alienoom@yandex.ru>
2023-03-24 19:45:31 +0500

    Поехали!

---
+++ app.py
@@ -0,0 +1,470 @@
+#!/usr/bin/python3
+
+# Kotodoski
+
+# Данный сервер предназначен для хостинга на бесплатном PythonAnywhere.
+# Написан криво, и для серьёзных игр НЕ годится.
+# Просто фигачим всё из config.py сюда
+from config import *
+
+from flask import Flask, request, Response
+from datetime import datetime, timezone, timedelta
+import hashlib
+import requests
+from threading import Lock
+
+app = Flask(__name__)
+lock = Lock()
+data: dict = None
+
+def get_current_datetime() -> datetime:
+    return datetime.now(timezone.utc)
+
+def get_current_date() -> datetime:
+    # Нам пофиг на *время* т.к. сервер мог быть запущен
+    # хоть в полдень, хоть в полночи, и нам нужно как-то нормально оперировать.
+    # По этому здесь время игнорируется, и уже нужные часы/минуты/недели добавляются
+    dtnow = get_current_datetime()
+    return datetime(dtnow.year, dtnow.month, dtnow.day)
+
+
+def get_next_reset_date(reset_type: str) -> datetime:
+    dtnow = get_current_date()
+
+    if not reset_type:
+        return None
+    elif reset_type == 'day':
+        return dtnow + timedelta(days=1)
+    elif reset_type == 'week':
+        return dtnow + timedelta(weeks=1)
+    elif reset_type == 'hour':
+        return dtnow + timedelta(hours=1)
+
+
+def init_if_not_already():
+    global data
+
+    if data is not None:
+        # уже...
+        return
+    
+    data = {}
+
+    for key, value in CONFIG_LEADERBOARD_INFO.items():
+        data[key] = {
+            'reset_every': value['reset_every'],
+            'reset_date': get_next_reset_date(value['reset_every']),
+            'sort_in_reverse': value['reverse_sort'],
+            'allow_overwrite': value['allow_overwrite'],
+            'max_entries': value['max_entries'],
+            'array': []
+        }
+        app.logger.info('Adding leaderboard with id of ', key, ' to the party...')
+    
+    app.logger.info('Initialized the leaderboard data')
+
+
+def reset_leaderboards_if_necessary():
+    global data
+
+    dtnow = get_current_date()
+
+    for key in data:
+        dtreset = data[key]['reset_date']
+        if not dtreset:
+            continue
+
+        if dtnow > dtreset:
+            app.logger.info('!!! Resetting leaderboard with id of ', key)
+            data[key]['array'].clear()
+            data[key]['reset_date'] = get_next_reset_date(data[key]['reset_every'])
+            app.logger.info('!!! Resetted ', key)
+        
+
+def pre_request():
+    init_if_not_already()
+    reset_leaderboards_if_necessary()
+
+
+def entry_sort_function(item) -> int:
+    return item['score']
+
+
+def impl_post_leaderboard(user_id: str, user_name: str, leaderboard_id: str, metadata: str, score: int) -> tuple[bool, str]:
+    global data
+    pre_request()
+
+    # нет смысла иметь отрицательные или нулевые очки в таблице рекордов...
+    if score is None or score <= 0:
+        return (False, '{"status":-1,"error":"param score is invalid"}')
+
+    if not user_id:
+        return (False, '{"status":-2,"error":"param user_id is invalid"}')
+    
+    if not user_name:
+        return (False, '{"status":-3,"error":"param user_name is invalid"}')
+    
+    if not leaderboard_id:
+        return (False, '{"status":-4,"error":"param leaderboard_id is invalid"}')
+    
+    if not (leaderboard_id in data):
+        return (False, '{"status":-5,"error":"leaderboard with given leaderboard_id does not exist"}')
+
+    board_array: list = data[leaderboard_id]['array']
+    for idx in range(len(board_array)):
+        # ищем есть ли мы уже в табличке
+        if board_array[idx]['user_id'] == user_id:
+            if data[leaderboard_id]['allow_overwrite']:
+                # если включена перезапись, то убираем старую запись
+                board_array.pop(idx)
+                break
+            else:
+                # если не включена, то возвращаем ошибку
+                return (False, '{"status":0,"error":"an entry for given user_id already exists"}')
+
+    # добавляем запись о пользователе в конец таблицы
+    metadatastr = None
+    if metadata is not None:
+        metadatastr = str(metadata)
+    board_array.append({
+        'user_id': user_id,
+        'user_name': user_name,
+        'score': score,
+        'timestamp': get_current_datetime().timestamp(),
+        'metadata': metadatastr,
+    })
+    # делаем пересортировку согласно параметрам таблицы
+    board_array.sort(
+        key = entry_sort_function,
+        # :) потому что доски почёта обычно сортируются от БкМ
+        reverse = not data[leaderboard_id]['sort_in_reverse']
+    )
+    # если назначен max_entries и есть лишние записи, убираем
+    max_entries = data[leaderboard_id]['max_entries']
+    if (max_entries is not None) and (max_entries > 0) and (len(board_array) > max_entries):
+        how_many = len(board_array) - max_entries
+        # обрубаем лишнее, здесь len всегда больше max_entries, и не равно ему.
+        for _ in range(how_many):
+            board_array.pop()
+    # перенаходим наш индекс
+    our_index = -1
+    for idx in range(len(board_array)):
+        if board_array[idx]['user_id'] == user_id:
+            our_index = idx
+            break
+    if our_index == -1:
+        # такого надеюсь не будет
+        app.logger.error('!!! FATAL ERROR in leaderboard data for ', leaderboard_id, ' please inspect!')
+        return (False, '{"status":-6,"error":"unable to find new entry index after sorting, WTF?!"}')
+    # йиппи!!!!1
+    return (True, '{"status":1,"error":"","new_entry_index":' + str(our_index) + '}')
+
+
+def impl_json_escape_string(s: str) -> str:
+    return \
+        str(s) \
+        .replace('\\', '\\\\') \
+        .replace('\n', '\\n') \
+        .replace('\r', '\\r') \
+        .replace('\t', '\\t') \
+        .replace('\b', '\\b') \
+        .replace('\f', '\\f') \
+        .replace('/', '\\/') \
+        .replace('"', '\\"')
+
+
+def impl_get_leaderboard(user_id: str, leaderboard_id: str, index_start: int, amount: int) -> tuple[bool, str]:
+    pre_request()
+
+    if not user_id:
+        return (False, '{"status":-1,"error":"param user_id is invalid"}')
+
+    if not leaderboard_id:
+        return (False, '{"status":-2,"error":"param leaderboard_id is invalid"}')
+    
+    if (index_start is None) or (index_start < -1):
+        return (False, '{"status":-3,"error":"param index_start is invalid"}')
+    
+    # index_start == -1 значит вывести относительно нашего пользователя
+    
+    if not (leaderboard_id in data):
+        return (False, '{"status":-4,"error":"leaderboard with given leaderboard_id does not exist"}')
+    
+    # amount <= 0 значит вывести до конца списка, если возможно.
+    if amount < 0:
+        return (False, '{"status":-5,"error":"param amount is invalid"}')
+
+    board_array: list = data[leaderboard_id]['array']
+    entries = len(board_array)
+    
+    # index_start == -1 значит искать относительно нас
+    if index_start == -1:
+        # собственно пытаемся найти "нас"
+        for idx in range(len(board_array)):
+            if board_array[idx]['user_id'] == user_id:
+                index_start = idx
+                break
+    
+    if index_start == -1:
+        # был дан индекс -1 (искать относительно нас), но мы "нас" так и не нашли!
+        return (False, '{"status":0,"error":"index_start is -1 but player with specified user_id is not present"}')
+
+    result = '{"status":1,"error":"","entries":['
+    wrote = 0
+    if amount == 0:
+        amount = entries - index_start
+    for idx in range(index_start, index_start + amount):
+        if idx >= entries:
+            break
+        elif wrote > 0:
+            result += ','
+
+        entry = board_array[idx]
+        result += '{"score":' + str(entry['score'])
+        result += ',"index":' + str(idx)
+        result += ',"timestamp":' + str(entry['timestamp'])
+        result += ',"user_id":"' + impl_json_escape_string(entry['user_id']) + '"'
+        result += ',"user_name":"' + impl_json_escape_string(entry['user_name']) + '"'
+        result += ',"metadata":"' + impl_json_escape_string(entry['metadata']) + '"'
+        result += '}'
+        wrote += 1
+
+    result += '],"amount":' + str(wrote) + ',"total":' + str(entries) + '}'
+    return (True, result)
+
+
+gas_session_cache = {}
+gas_lock = Lock()
+
+
+def do_gas_sign_sort_function(item) -> str:
+    return item['key']
+
+
+def do_gas_sign(contents: dict[str, str], secret: str) -> str:
+    # сортировка по ключам
+    templist = []
+    for key, value in contents.items():
+        templist.append({'key': key, 'value': value})
+    templist.sort(key=do_gas_sign_sort_function)
+    # создание строки для подписи
+    tempstr = ''
+    for item in templist:
+        tempstr += item['key'] + '=' + item['value']
+    tempstr += secret
+    # переводим в utf-8 байтики
+    tempstru8 = tempstr.encode('utf-8')
+    # создание хэша строки
+    sign = hashlib.md5(tempstru8).hexdigest()
+    return sign
+
+
+def do_gas_request(gas_uid: str, gas_hash: str, gas_ip: str) -> tuple[bool, str]:
+    # -> bool - True / str это имя пользователя, False / это json с ошибкой.
+
+    with gas_lock:
+        if not gas_uid:
+            return (False, '{"status":-10,"error":"param gas_uid is invalid"}')
+        
+        if not gas_hash:
+            return (False, '{"status":-11,"error":"param gas_hash is invalid"}')
+        
+        if not gas_ip:
+            return (False, '{"status":-12,"error":"param gas_ip is invalid"}')
+        
+        gas_gmr_id = str(CONFIG_GAS_GMR_ID)
+        if not gas_gmr_id:
+            return (False, '{"status":-13,"error":"param gas_gmr_id is invalid"}')
+        
+        gas_secret = CONFIG_GAS_SECRET
+        if not gas_secret:
+            return (False, '{"status":-14,"error":"param gas_secret is invalid"}')
+        
+        # чистим кэш если он забит
+        if len(gas_session_cache) > CONFIG_GAS_MAX_CACHE_ENTRIES:
+            app.logger.info('Clearing GAS session cache...')
+            gas_session_cache.clear()
+            app.logger.info('GAS session cache has been emptied')
+        
+        # если мы уже авторизованы то получаем имя пользователя
+        cache_key = gas_gmr_id + gas_secret + gas_uid + gas_hash + gas_ip
+        if cache_key in gas_session_cache:
+            # нашли в кэше
+            return (True, gas_session_cache[cache_key])
+        
+        gas_sign = do_gas_sign({
+            'uid': gas_uid,
+            'hash': gas_hash,
+            'ip': gas_ip,
+            'appid': gas_gmr_id
+        }, gas_secret)
+        if not gas_sign:
+            return (False, '{"status":-15,"error":"failed to calculate gas_sign"}')
+        
+        gas_url = f'https://vkplay.ru/app/{gas_gmr_id}/gas?uid={gas_uid}&hash={gas_hash}&ip={gas_ip}&sign={gas_sign}'
+
+        try:
+            ok = requests.get(gas_url, headers={'User-Agent': CONFIG_SERVER_USER_AGENT})
+            if ok.status_code >= 400:
+                return (False, '{"status":-16,"error":"gas api request forbidden"}')
+            ok_json = ok.json()
+            ok_json_status = ok_json['status']
+            if ok_json_status != 'ok':
+                return (False, '{"status":-17,"error":"gas api status failed"}')
+            
+            # TODO: пихать что-то полезнее чем 'ok'?
+            gas_session_cache[cache_key] = ok_json_status
+            return (True, cache_key)
+        except:
+            return (False, '{"status":-18,"error":"gas api request failed"}')
+
+
+vksteam_ticket_cache = {}
+vksteam_lock = Lock()
+
+
+def do_vksteam_verify_ticket(ticket: str, user_id: str) -> tuple[bool, str]:
+    with vksteam_lock:
+        if not user_id:
+            return (False, '{"status":-20,"error":"param user_id is invalid"}')
+        
+        if not ticket:
+            return (False, '{"status":-21,"error":"param vksteam_ticket is invalid"}')
+        
+        if len(vksteam_ticket_cache) > CONFIG_VKSTEAM_MAX_CACHE_ENTRIES:
+            app.logger.info('Clearing VKSteam ticket cache...')
+            vksteam_ticket_cache.clear()
+            app.logger.info('VKSteam ticket cache cleared')
+
+        if ticket in vksteam_ticket_cache:
+            return (True, vksteam_ticket_cache[ticket])
+
+        url = f'https://api.vkplay.ru/steam/ISteamUserAuth/AuthenticateUserTicket/v1/?key={CONFIG_VKSTEAM_KEY}&ticket={ticket}&appid={CONFIG_VKSTEAM_APP_ID}'
+
+        try:
+            ok = requests.get(url, {'User-Agent': CONFIG_SERVER_USER_AGENT})
+            if ok.status_code >= 400:
+                return (False, '{"status":-22,"error":"vksteam api request forbidden"}')
+            ok_json = ok.json()
+            if ok_json['response']['params']['result'] != 'OK':
+                return (False, '{"status":-23,"error":"vksteam api result is not OK"}')
+            steamid = ok_json['response']['params']['steamid']
+            ownersteamid = ok_json['response']['params']['ownersteamid']
+            if steamid != user_id and ownersteamid != user_id:
+                return (False, '{"status":-24,"error":"vksteam api user id mismatch"}')
+            vksteam_ticket_cache[ticket] = user_id
+            return (True, user_id)
+        except:
+            return (False, '{"status":-25,"error":"vksteam api request failed"}')
+
+
+@app.route('/v1/api/post', methods=['POST'])
+def post_leaderboard():
+    leaderboard_id = request.form.get('leaderboard_id', type=str)
+    metadata = request.form.get('metadata', type=str)
+    score = request.form.get('score', type=int)
+
+    user_id = request.form.get('user_id', type=str)
+    user_name = request.form.get('user_name', type=str)
+    if CONFIG_USE_GAS:
+        gas_uid = request.form.get('gas_uid')
+        gas_hash = request.form.get('gas_hash')
+        gas_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+        gas_result = do_gas_request(gas_uid, gas_hash, gas_ip)
+        if not gas_result[0]:
+            return Response(response=gas_result[1], status=401, content_type='application/json; charset=utf-8')
+        if not user_id:
+            # разрешить не указывать user_id если он уже известен из GAS
+            user_id = gas_uid
+    elif CONFIG_USE_VKSTEAM:
+        vksteam_ticket = request.form.get('vksteam_ticket')
+        vksteam_result = do_vksteam_verify_ticket(vksteam_ticket, user_id)
+        if not vksteam_result[0]:
+            return Response(response=vksteam_result[1], status=401, content_type='application/json; charset=utf-8')
+    
+    with lock:
+        rv = impl_post_leaderboard(
+            user_id,
+            user_name,
+            leaderboard_id,
+            metadata,
+            score
+        )
+
+    if rv[0]:
+        httpstatus = 200
+    else:
+        httpstatus = 400
+    
+    return Response(response=rv[1], status=httpstatus, content_type='application/json; charset=utf-8')
+
+
+@app.route('/v1/api/get', methods=['GET'])
+def get_leaderboard():
+    leaderboard_id = request.args.get('leaderboard_id', type=str)
+    index_start = request.args.get('index_start', type=int)
+    amount = request.args.get('amount', type=int)
+
+    user_id = request.args.get('user_id', type=str)
+    if CONFIG_USE_GAS:
+        gas_uid = request.args.get('gas_uid')
+        gas_hash = request.args.get('gas_hash')
+        gas_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+        gas_result = do_gas_request(gas_uid, gas_hash, gas_ip)
+        if not gas_result[0]:
+            return Response(response=gas_result[1], status=401, content_type='application/json; charset=utf-8')
+        if not user_id:
+            user_id = gas_uid
+    elif CONFIG_USE_VKSTEAM:
+        vksteam_ticket = request.args.get('vksteam_ticket')
+        vksteam_result = do_vksteam_verify_ticket(vksteam_ticket, user_id)
+        if not vksteam_result[0]:
+            return Response(response=vksteam_result[1], status=401, content_type='application/json; charset=utf-8')
+
+    with lock:
+        rv = impl_get_leaderboard(
+            user_id,
+            leaderboard_id,
+            index_start,
+            amount
+        )
+
+    if rv[0]:
+        httpstatus = 200
+    else:
+        httpstatus = 400
+    
+    return Response(response=rv[1], status=httpstatus, content_type='application/json; charset=utf-8')
+
+
+@app.route('/v1/api/admin_action', methods=['GET'])
+def get_admin_action():
+    global data
+
+    ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    app.logger.info('admin request from ip address ' + ip)
+
+    if not CONFIG_ADMIN_SECRET:
+        return 'admin methods are disabled, izvinite!'
+
+    secret = request.args.get('secret', type=str)
+    if secret is None or secret != CONFIG_ADMIN_SECRET:
+        return 'ne-a, izvinite'
+    
+    req = request.args.get('action', type=str)
+    if not req:
+        return 'admin: secret is correct, but no action was given'
+    elif req == 'reset':
+        with lock:
+            data = None
+            app.logger.info('Admin leaderboards reset!')
+        return 'reset: successful'
+    else:
+        return 'unknown admin api action o_O?'
+
+
+if __name__ == '__main__':
+    app.logger.info('!!! Starting a Flask application for local debugging...')
+    app.logger.info('Kotodoski debug')
+    app.run(port=8080, debug=True)
+
---
+++ config_template.py
@@ -0,0 +1,52 @@
+
+#
+# Скопируй меня как config.py и запусти сервак!
+#
+
+# -- КОНФИГУРАЦИЯ       -- #
+# bool, использовать GAS для проверки запросов, или же не проверять авторизацию?
+CONFIG_USE_GAS = False
+# int, Игра -> Системные свойства -> Состояние -> ID Игры (GMRID)
+CONFIG_GAS_GMR_ID = 0
+# str, GAS секрет из системных свойств??
+CONFIG_GAS_SECRET = ''
+# int, сколько GAS сессий нужно держать в кэше?
+CONFIG_GAS_MAX_CACHE_ENTRIES = 1000
+# bool, использовать VKSteam для авторизации или нет?
+CONFIG_USE_VKSTEAM = True
+# int, Игра -> Системные свойства -> Режим эмуляции Steam -> ID для эмуляции Steam
+CONFIG_VKSTEAM_APP_ID = 2000000
+# str, Игра -> Системные свойства -> Режим эмуляции Steam -> Секрет для эмуляции Steam API
+CONFIG_VKSTEAM_KEY = 'abcdef'
+# int, сколько тикетов нужно держать в кэше?
+CONFIG_VKSTEAM_MAX_CACHE_ENTRIES = 1000
+# str, каким User-Agentом представляться системе GAS или VKSteam?
+CONFIG_SERVER_USER_AGENT = 'AverageGASEnjoyer/1.0 (WeLoveCats; Cat-like)'
+# str, секрет для admin_ методов
+CONFIG_ADMIN_SECRET = 'yaga.yaga.yaguar.moi.volshebniy.nektar'
+# dict, конфигурация досок почёта, key - идентификатор доски.
+CONFIG_LEADERBOARD_INFO = {
+    # daily runs
+    'board_daily': {
+        # str, 'day' - сброс каждый день, 'week' - каждую неделю, 'hour' - час, None - не сбрасывать
+        'reset_every': 'day',
+        # bool, True - сортировать от меньшего к большему
+        'reverse_sort': False,
+        # bool, разрешать ли публиковать запись если данный игрок УЖЕ публиковал?
+        'allow_overwrite': False,
+        # int, макс. кол-во записей, None - не ограничено (опасно!)
+        'max_entries': 1000
+    },
+    # weekly runs
+    'board_weekly': {
+        # str, 'day' - сброс каждый день, 'week' - каждую неделю, 'hour' - час, None - не сбрасывать
+        'reset_every': 'week',
+        # bool, True - сортировать от меньшего к большему
+        'reverse_sort': False,
+        # bool, разрешать ли публиковать запись если данный игрок УЖЕ публиковал?
+        'allow_overwrite': False,
+        # int, макс. кол-во записей, None - не ограничено (опасно!)
+        'max_entries': 1000
+    }
+}
+# -- КОНЕЦ КОНФИГУРАЦИИ -- #
